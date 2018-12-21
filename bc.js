48d3296de6cf477c5f8d6c8d6f40f263ec2a302a
youssefeno <45043899+youssefeno@users.noreply.github.com>
2018-12-21 13:30:08 +0200

    Update bc.js

--- bc.js
+++ bc.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = ".bc";
+var prefix = ".";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -29,4 +29,4 @@ client.user.setStatus("dnd")
 
 
 
-client.login("NTIyNzUwNzM0NjYwNDAzMjAz.DvPhIQ.RwXzXj0hyDBl9ZN4itriKHPpjwc");
+client.login("NTIyNzUwNzM0NjYwNDAzMjAz.Dv5eqQ.MPtGSDylvn3_yfSWVO2QhWM1qoE");
